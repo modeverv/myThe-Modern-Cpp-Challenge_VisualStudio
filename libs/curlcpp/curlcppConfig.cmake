7990e408cf65f7aae6c11bea46c0cbebb9781c50
池畑 成二郎 <is40701@itec.local>
2019-02-15 10:44:20 +0900

    initial

---
+++ libs/curlcpp/curlcppConfig.cmake
@@ -0,0 +1,5 @@
+# curlcppConfig.cmake - Configuration file for external projects.
+
+set(CURLCPP_INCLUDE_DIRS "C:/Users/is40701.ITEC/Desktop/The-Modern-Cpp-Challenge-master/libs/curlcpp/include")
+set(CURLCPP_LIBRARY_DIRS "C:/Users/is40701.ITEC/Desktop/The-Modern-Cpp-Challenge-master/build/libs/curlcpp/src")
+set(CURLCPP_LIBRARIES "$<TARGET_FILE:>")
